2d7031fe72f5980beebc8cb8019e7c4162f600b2
younggyeol <vmflr2@gmail.com>
2022-12-27 00:06:42 +0900

    mongodb/url connection_string 수정

--- plugins/mongodb.js
+++ plugins/mongodb.js
@@ -5,11 +5,10 @@ const fp = require('fastify-plugin')
 const mongodb = require('@fastify/mongodb')
 
 module.exports = fp(async function (fastify, opts) {
-  console.log(process.env.CONNETION_STRING)
   
   fastify.register(mongodb, {
     forceClose: true,
-    url: 'mongodb+srv://youngjun:73ZjlgwnM1GB3Uia@cluster0.sjrpyqv.mongodb.net/vote?retryWrites=true&w=majority'
+    url: process.env.CONNECTION_STRING
   } )
   
   console.log('mongobd 플러그인 등록 성공')
--- routes/votes/index.js
+++ routes/votes/index.js
@@ -1,5 +1,5 @@
 'use strict'
-//모든 생성된 투표 확인
+//모든 생성된 투표 조회
 module.exports = async function (fastify, opts) {
   fastify.get('/', async function (request, reply) {
     
@@ -13,7 +13,28 @@ module.exports = async function (fastify, opts) {
      .send(result)
   })
 
-  // 생성된 투표 확인
+  //특정분류의 투표를 조회  ##작성중
+  fastify.get('?category=:category', async function (request, reply) {
+    
+    const votes = this.mongo.db.collection('votes')
+    
+    const result = await votes.find({category:request.params.category}).toArray()
+   
+    if(result){
+    reply
+     .code(200)
+     .header('content-type', 'application/json')
+     .send(result)
+    }
+    else {
+      reply
+       .code(404)
+       .header('content-type', 'text/plain')
+       .send('Not Found')
+    }
+  })
+
+  // 생성된 투표 조회
   fastify.get('/:voteId', async function (request, reply) {
     
     const votes = this.mongo.db.collection('votes')
@@ -34,13 +55,37 @@ module.exports = async function (fastify, opts) {
     }
   })
 
-  //생성된 투표에서 모든선택지 확인
+  //생성된 투표에서 모든선택지 조회
   fastify.get('/options/:voteId', async function (request, reply) {
     
     const ops = this.mongo.db.collection('options')
   
     const result = await ops.find({voteId:request.params.voteId}).toArray()
  
+    if(result){
+    reply
+     .code(200)
+     .header('content-type', 'application/json')
+     .send(result)
+    }
+    else {
+      reply
+       .code(404)
+       .header('content-type', 'text/plain')
+       .send('Not Found')
+    }
+  })
+
+  //투표의 결과를 확인   ##작성중
+  fastify.get('/result/:voteId', async function (request, reply) {
+    
+    const ops = this.mongo.db.collection('options')
+    const count = await ops.find({voteId:request.params.voteId}).count()
+
+    const result = await ops.find({voteId:request.params.voteId}).toArray()
+    
+    console.log(count)
+    
     if(result){
     reply
      .code(200)
